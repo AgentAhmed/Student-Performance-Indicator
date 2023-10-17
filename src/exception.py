276eec365005014af16e9f328039043a04a97d1c
Ahmed <ahmed.mca18@gmail.com>
2023-10-17 16:45:49 +0530

    updated

--- src/exception.py
+++ src/exception.py
@@ -22,7 +22,7 @@ class CustomException(Exception):
         return self.error_message
 
 
-#if __name__ == "__main__":   # doing this to check exception
+# if __name__ == "__main__":   # doing this to check exception
 
 #     try:
 #         a = 1/0
